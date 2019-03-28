7e339bfcd6a2428e8ef5594b54d8d71ab271a22f
William Mitchell <wemitchell3@gmail.com>
2019-03-28 14:47:32 -0500

    Uploading finised fetching others data practice.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Fetching Data with JavaScript
+Once you have retrieved all of the foods from your own Food API, display each one of them in the DOM. Create an HTML representation of each food which will display the name of the food, its type, and its ethnicity.
+
+1. Create a DOM element in your index.html with a class of foodList.
+1. Create a function which returns a string template. The template is the HTML representation for a food item.
+1. Create a function that inserts an HTML representation of a food into the DOM
+1. Create a new barcode key for each of the items in your API, and provide the correct barcode value that you found in your search of the Open Food Facts database.
+1. Your job is to query the Open Food Facts API for each of your products, and list the following additional information.
+    1. Ingredients
+    1. Country of origin
+    1. Calories per serving
+    1. Fat per serving
+    1. Sugar per serving
--- src/scripts/food.js
+++ src/scripts/food.js
@@ -2,25 +2,50 @@ const buildFoodHtml = foodObject => {
     const foodArticle = buildElement("article")
     foodArticle.classList.add("food")
     foodArticle.appendChild(buildElement("h3", undefined, foodObject.name))
-    foodArticle.appendChild(buildElement("p", undefined, foodObject.ethnicity))
-    foodArticle.appendChild(buildElement("p", undefined, foodObject.category))
+    foodArticle.appendChild(buildElement("p", undefined, "Ethnicity: " + foodObject.ethnicity))
+    foodArticle.appendChild(buildElement("p", undefined, "Category: " + foodObject.category))
+    foodArticle.appendChild(buildElement("p", undefined, "Ingredients: " + foodObject.ingredients))
+    foodArticle.appendChild(buildElement("p", undefined, "Origin Country:" + foodObject.country))
+    foodArticle.appendChild(buildElement("p", undefined, "Calories: " + foodObject.calories))
+    foodArticle.appendChild(buildElement("p", undefined, "Fat: " + foodObject.fat))
+    foodArticle.appendChild(buildElement("p", undefined, "Sugar: " + foodObject.sugar))
 
     return foodArticle
 }
 
+// const listFoods = () => {
+//     getAllFoods().then(foodsArray => {
+//         console.table("foodsArray", foodsArray)
+//         let allFoodsFragment = document.createDocumentFragment()
+
+//         foodsArray.forEach(food => {
+//             let foodHtml = buildFoodHtml(food)
+//             allFoodsFragment.appendChild(foodHtml)
+//         })
+//         const allFoodsSection = document.querySelector("#food")
+//         allFoodsSection.appendChild(allFoodsFragment)
+//     })
+// }
+
 const listFoods = () => {
-    getAllFoods().then(foodsArray => {
-        console.table("foodsArray", foodsArray)
-        let allFoodsFragment = document.createDocumentFragment()
-
-        foodsArray.forEach(food => {
-            let foodHtml = buildFoodHtml(food)
-            allFoodsFragment.appendChild(foodHtml)
+    fetch(`http://localhost:8088/food`)
+        .then(food => food.json())
+        .then(foodsArray => {
+            let allFoodsFragment = document.createDocumentFragment()
+            foodsArray.forEach(food => {
+                fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                    .then(response => response.json())
+                    .then(productInfo => {
+                        food.ingredients = productInfo.product.ingredients_text;
+                        food.country = productInfo.product.countries;
+                        food.calories = Math.ceil((productInfo.product.nutriments.energy_serving) / 4.184);
+                        food.fat = productInfo.product.nutriments.fat_serving;
+                        food.sugar = productInfo.product.nutriments.sugars_serving;
+                        let foodHtml = buildFoodHtml(food)
+                        allFoodsFragment.appendChild(foodHtml)
+                        const allFoodsSection = document.querySelector("#food")
+                        allFoodsSection.appendChild(allFoodsFragment)
+                    })
+            })
         })
-        const allFoodsSection = document.querySelector("#food")
-        allFoodsSection.appendChild(allFoodsFragment)
-    })
-
 }
-
-
